a2622b3f790bec9eabffd446f48d546161db6998
Ich Van Ninh <ichnv2610@gmail.com>
2017-03-10 18:46:02 +0700

    Init project

---
+++ Proteam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proteam
+//
+//  Created by IchIT on 3/10/17.
+//  Copyright © 2017 IchIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
